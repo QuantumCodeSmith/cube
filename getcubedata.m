b33b48454fc1b5b98dc46cd645a8a669ab33fa36
QuantumCodeSmith <143517249+QuantumCodeSmith@users.noreply.github.com>
2023-11-07 10:32:56 -0500

    Starting files

---
+++ getcubedata.m
@@ -0,0 +1,49 @@
+clear; 
+% Get hyperspectral data from cube file(s)
+% created by Chet Szwejkowski, Application Scientist, Surface Optics Corp.
+% on September 28, 2018
+%
+% Use:
+%   Change directory to location where this file is saved
+%   Type '[cube] = getdata();' in MATLAB Command window
+%   Follow prompts
+%   File names and data are saved in structure called 'cube'
+
+
+getdata();
+output = ans.data{1,1};
+band64 = output(:,64,:);
+band64 = reshape(band64, 520, 696);
+
+function [cube] = getdata()
+  % settings
+  header = 32768/2;
+  samples = 520; %height
+  lines = 696; %width
+  bands = 128; %number of bands
+  datalength = samples*lines*bands;
+  
+  % user selects data file(s)
+  homepath = cd();
+  [fname,fpath] = uigetfile('*.cube','Choose data file(s)','MultiSelect','on');
+  if iscell(fname)
+    fname = fname;
+  else
+    fname = {fname};
+  end
+  num_files = size(fname,2);
+  cd(fpath)
+    
+  % loop through files to extract data
+  cube.files = fname;
+  for ii = 1:num_files
+    fid = fopen(fname{ii});
+    dummy = fread(fid,inf,'uint16');
+    dummy = dummy(header+1:header+datalength);
+    cube.data{ii,1} = reshape(dummy,[samples,bands,lines]);
+  end
+  
+  cd(homepath)
+  
+end
+
